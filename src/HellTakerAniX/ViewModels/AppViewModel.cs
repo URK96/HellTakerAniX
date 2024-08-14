d125deab0381fce79b733ae4537bdfc0d8ccc4b8
URK96 <chlwlsgur96@hotmail.com>
2024-08-14 16:43:12 +0900

    [Add] Add character view command when click tray icon "Add Character" menu

--- src/HellTakerAniX/ViewModels/AppViewModel.cs
+++ src/HellTakerAniX/ViewModels/AppViewModel.cs
@@ -3,6 +3,14 @@ namespace HellTakerAniX.ViewModels;
 public partial class AppViewModel : ObservableObject
 {
     [RelayCommand]
+    private void AddCharacterView()
+    {
+        CharacterAniWindow window = new();
+
+        window.Show();
+    }
+
+    [RelayCommand]
     private void ExitApp()
     {
         Environment.Exit(0);
