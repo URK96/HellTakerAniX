31715f4891a499d88e09e13398abb3aef66a6c19
URK96 <chlwlsgur96@hotmail.com>
2024-10-12 21:56:54 +0900

    [Modify] Change running animation window implementation

--- src/HellTakerAniX/MainWindow.axaml.cs
+++ src/HellTakerAniX/MainWindow.axaml.cs
@@ -4,8 +4,19 @@ namespace HellTakerAniX;
 
 public partial class MainWindow : Window
 {
+    private readonly List<AnimationWindow> _currentRunningAnimationWindowList = [];
+
     public MainWindow()
     {
         InitializeComponent();
+        CreateNewAnimationWindow();
+    }
+
+    private void CreateNewAnimationWindow()
+    {
+        AnimationWindow window = new();
+
+        window.Show();
+        _currentRunningAnimationWindowList.Add(window);
     }
 }
